ef4faf3bc16e3822c7ceb01a2cd2cc34820fe64e
fazibear <fazibear@gmail.com>
2014-10-22 15:02:00 +0200

    new icon, little bit of refactoring

--- Support/lint.rb
+++ Support/lint.rb
@@ -2,29 +2,45 @@
 
 require 'rubocop'
 
-processed_source = RuboCop::ProcessedSource.from_file(ENV['TM_FILEPATH'])
-team = RuboCop::Cop::Team.new(
-  RuboCop::Cop::Cop.all,
-  RuboCop::ConfigStore.new.for(processed_source.path),
-  {}
-)
-offenses = team.inspect_file(processed_source)
-
-messages = {}
-
-offenses.each do |offence|
-  (messages[offence.line] ||= []) << offence.message.gsub('`', "").gsub(',',' ')
+def log(msg)
+  require 'logger'
+  logger = Logger.new('/tmp/rubocop_bundle.log')
+  logger.info msg
 end
 
-args = ['--clear-mark=note']
-
-messages.each do |line, messages|
-  args << "--set-mark=note:\"#{messages.join(' ')}\""
-  args << "--line=#{line}"
+def offences(file)
+  processed_source = RuboCop::ProcessedSource.from_file(file)
+  team = RuboCop::Cop::Team.new(
+    RuboCop::Cop::Cop.all,
+    RuboCop::ConfigStore.new.for(processed_source.path),
+    {}
+  )
+  team.inspect_file(processed_source)
 end
 
-args << ENV['TM_FILEPATH'].inspect
+def messages(offences)
+  messages = {}
+  offences.each do |offence|
+    (messages[offence.line] ||= []) << offence.message.gsub('`', "'").gsub(',', ' ')
+  end
+  messages
+end
 
-cmd = "#{ENV['TM_MATE']} #{args.join(' ')}"
+def command(messages)
+  warrning_icon = "#{ENV['TM_BUNDLE_SUPPORT']}/warrning.pdf".inspect
+  args = ["--clear-mark=#{warrning_icon}"]
 
+  messages.each do |line, message|
+    args << "--set-mark=#{warrning_icon}:#{message.join(' ').inspect}"
+    args << "--line=#{line}"
+  end
+
+  args << ENV['TM_FILEPATH'].inspect
+
+  "#{ENV['TM_MATE']} #{args.join(' ')}"
+end
+
+cmd = command(messages(offences(ENV['TM_FILEPATH'])))
+
+# log cmd
 exec cmd

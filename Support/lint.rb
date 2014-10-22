24cc72de1ec4f5a4424be43b1b6c7f3e697338c0
fazibear <fazibear@gmail.com>
2014-10-22 11:31:59 +0200

    fix rubocop warrnings

--- Support/lint.rb
+++ Support/lint.rb
@@ -4,17 +4,17 @@ require 'rubocop'
 
 processed_source = RuboCop::ProcessedSource.from_file(ENV['TM_FILEPATH'])
 team = RuboCop::Cop::Team.new(
-  RuboCop::Cop::Cop.all, 
-  RuboCop::ConfigStore.new.for(processed_source.path), 
+  RuboCop::Cop::Cop.all,
+  RuboCop::ConfigStore.new.for(processed_source.path),
   {}
 )
 offenses = team.inspect_file(processed_source)
 
-args = [ "--clear-mark=error" ]
+args = ['--clear-mark=error']
 
 offenses.each do |offence|
   args << "--set-mark=error:\"#{offence.message}\""
-  args << "--line=#{offence.line}" 
+  args << "--line=#{offence.line}"
 end
 
 args << ENV['TM_FILEPATH'].inspect
